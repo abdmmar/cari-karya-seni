0b4b2a978b11a6e669df6850dcc5988a5f238ccc
abdmmar <opowae909@gmail.com>
2020-11-11 10:36:27 +0700

    Add search input

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import Art from './components/art';
+import Art from "./components/art";
 
-const API_KEY = `&apikey=${process.env.REACT_APP_API_KEY}`;
+const API_KEY = `apikey=${process.env.REACT_APP_API_KEY}&`;
 
-const GET_ALL_CLASSS = `https://api.harvardartmuseums.org/classification?size=59${API_KEY}`
-const GET_OBJECT = 'https://api.harvardartmuseums.org/object?'
+const GET_ALL_CLASSS = `https://api.harvardartmuseums.org/classification?size=59${API_KEY}`;
+const GET_OBJECT = "https://api.harvardartmuseums.org/object?";
 
 function App() {
   const [art, setArt] = useState([]);
   const [worksCount, setWorksCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(`${GET_OBJECT}classification=26&size=50${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setArt(data.records);
-      setWorksCount(data.info.totalrecords);
-    });
-    
+    getArts('classification=any&size=50')
   }, []);
+
+  const getArts = (query) => {
+    fetch(`${GET_OBJECT}${API_KEY}${query}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setArt(data.records);
+        setWorksCount(data.info.totalrecords);
+      });
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    getArts(`q=${searchTerm}&size=50`);
+  };
+
+  const handleOnChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <main>
+      <header className="search-box">
+        <form onSubmit={handleOnSubmit}>
+          <label htmlFor="search">
+            <h2>Telusuri Koleksi Karya Seni</h2>
+          </label>
+          <input
+            type="search"
+            id="search"
+            placeholder="Cari berdasarkan kata kunci, judul atau seniman"
+            className="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
       <p className="workCount">Showing {worksCount} Works</p>
       <div className="art-container">
-        {art.map(art => 
-          art.primaryimageurl &&
-          <Art key={art.id} id ={art.id} data={art} />
+        {art.map(
+          (art) =>
+            art.primaryimageurl && <Art key={art.id} id={art.id} data={art} />
         )}
       </div>
     </main>
--- src/components/art.js
+++ src/components/art.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Art = ({id, data}) => {
-    const people = data.people === undefined ? 'Unknown' : data.people[0].name;
+const Art = ({ id, data }) => {
+  const people = data.people === undefined ? "Unknown" : data.people[0].name;
 
-    return (
-        <article id = {id} className="art">
-            <img src={data.primaryimageurl} alt={data.title} />
-            <div className="art-info">
-                <p className="artist">{people}</p>
-                <h3 className="art-title">{data.title}</h3>
-                <p className="art-class-medium">{data.classification} • {data.medium} {data.datebegin !== 0 ? `• ${data.datebegin}` : ''}</p>
-            </div>
-        </article>    
-    )
+  return (
+    <article id={id} className="art">
+      <img loading="lazy" src={data.primaryimageurl} alt={data.title} />
+      <div className="art-info">
+        <p className="artist">{people}</p>
+        <h3 className="art-title">{data.title}</h3>
+        <p className="art-class-medium">
+          {data.classification} • {data.medium}{" "}
+          {data.datebegin !== 0 ? `• ${data.datebegin}` : ""}
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default Art;
